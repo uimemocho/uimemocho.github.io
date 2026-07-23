b1544ffd045c5afc6ad91a2ea520058a09e15a1c
u <u@yunoMacBook-Pro.local>
2026-07-23 17:23:02 +0900

    Remove mistaken site bottom bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata, Viewport } from "next";
+import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -12,12 +12,6 @@ export const metadata: Metadata = {
     type: "website",
     locale: "ja_JP",
   },
-};
-
-export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1,
-  viewportFit: "cover",
 };
 
 export default function RootLayout({
